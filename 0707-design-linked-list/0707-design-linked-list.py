6ccdd85401e35a9b10531fab10d1790628fceaee
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-10-22 16:38:21 -0400

    Time: 567 ms (19.10%), Space: 14.8 MB (27.88%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -1,5 +1,4 @@
 class Node:
-    
     def __init__(self, val):
         self.val = val
         self.next = None
@@ -8,21 +7,23 @@ class Node:
 class MyLinkedList:
 
     def __init__(self):
-        self._head = Node(0)  # 虚拟头部节点
-        self._count = 0  # 添加的节点数
+        self.head = Node(0)
+        self.count = 0
 
     def get(self, index: int) -> int:
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
-        if 0 <= index < self._count:
-            node = self._head
-            for _ in range(index + 1):
-                node = node.next
-            return node.val
-        else:
+        if index < 0 or index >= self.count: 
             return -1
-
+        else:
+            cur = self.head
+            for _ in range(index+1): # +1 因为算上了 dummy head
+                cur = cur.next
+            return cur.val
+        # (0) - 6 - 8 - 9 - 3 - 4 - 6  get index 4 (should return 4)
+        # cur should move 5 steps, so the for loop should be (0, index+1)
+    
     def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
@@ -33,39 +34,43 @@ class MyLinkedList:
         """
         Append a node of value val to the last element of the linked list.
         """
-        self.addAtIndex(self._count, val)
+        self.addAtIndex(self.count, val)
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
         """
-        if index < 0:
+        if index > self.count: 
+            return -1
+        if index < 0: 
             index = 0
-        elif index > self._count:
-            return
-
-        # 计数累加
-        self._count += 1
-
-        add_node = Node(val)
-        prev_node, current_node = None, self._head
-        for _ in range(index + 1):
-            prev_node, current_node = current_node, current_node.next
-        else:
-            prev_node.next, add_node.next = add_node, current_node
+        cur = self.head
+        for _ in range(index):
+            cur = cur.next 
+        new_node = Node(val)
+        new_node.next = cur.next # should first connect new node to cur.next, then connect cur.next to new_node
+        cur.next = new_node   # because if you do it reversely, cur.next's address will be lost
+    
+        self.count += 1
+        
+        # (0) - 6 - 8 - 9 - 3 - 4 - 6  add at index 4
+        # should stop one node before, namely, index 3
+        # hence, we don't need to increment the index
+        # for loop should be (0, index)
+        # increament self.count by 1
+            
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
-        if 0 <= index < self._count:
-            # 计数-1
-            self._count -= 1
-            prev_node, current_node = None, self._head
-            for _ in range(index + 1):
-                prev_node, current_node = current_node, current_node.next
-            else:
-                prev_node.next, current_node.next = current_node.next, None
+        if index < 0 or index >= self.count: return
+        else:
+            pre = self.head
+            for _ in range(index):
+                pre = pre.next
+            pre.next = pre.next.next
+            self.count -= 1
 
 
 
