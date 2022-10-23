c9aeaa711dc3ebee790a97459db0bc121b4a8630
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-10-22 22:34:13 -0400

    Time: 3287 ms (21.50%), Space: 17.1 MB (31.20%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -0,0 +1,26 @@
+import functools
+class Solution(object):
+    def numMatchingSubseq(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: int
+        """
+        count = 0
+        for word in words:
+            if self.compareString(word, s): 
+                count += 1
+        return count
+    
+    @functools.lru_cache
+    def compareString(self, word, s): # compare string method is problemetic
+        if not s: return False # why return False
+        i = 0
+        j = 0
+        while i < len(s) and j < len(word):
+            if s[i] == word[j]:
+                i += 1
+                j += 1
+            else:
+                i += 1
+        return True if j == len(word) else False
