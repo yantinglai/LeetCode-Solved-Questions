d0d864f6f192c8cfb7a616ec50a5107d3b55008e
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-10-28 03:47:14 -0400

    Time: 78 ms (6.34%), Space: 13.9 MB (78.55%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -6,24 +6,21 @@ class Solution:
     
         def backtracking(startIndex, k, n, total):
             if total > n: return 
-            if total == n and len(path) == k:
-                res.append(path[:])
-                return
+            if len(path) == k and total == n:
+                    res.append(path[:])
+                    return
             for i in range(startIndex, 10):
-                if i in path:
-                    continue
                 total += i
                 path.append(i)
-                backtracking(startIndex+1, k, n,total)
+                backtracking(i+1, k, n,total)
                 path.pop()
-                total -= i
-                i += 1    
+                total -= i 
                 
         backtracking(1,k,n,0)
-        final_res = []
+#         final_res = []
         
-        for item in res:
-            check = set(item)
-            if check not in final_res:
-                final_res.append(check)
-        return final_res
+#         for item in res:
+#             check = set(item)
+#             if check not in final_res:
+#                 final_res.append(check)
+        return res
