040ea7435f645d35eebba5ece533d12e3db06a82
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-11-05 19:31:45 -0400

    Time: 69 ms (11.09%), Space: 14 MB (27.59%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
@@ -12,18 +12,24 @@ class Solution:
         
         d = collections.defaultdict(list)
         
-        def getHeight(root,d):
+        def getHeight(root):
             if not root: 
                 return 0 
-            left = getHeight(root.left,d)
-            right = getHeight(root.right,d)
+            left = getHeight(root.left)
+            right = getHeight(root.right)
             cur_height = max(left,right) + 1
             d[cur_height].append(root.val)
+            print(d)
             return cur_height
         
-        getHeight(root,d)
+        getHeight(root)
         return d.values()
         
+        # 1: left. :2  right: 1 - max(2,1) -- 2+1     
+        # 2: left = 1.   right = 1   
+        # 4: left = 0 , right =0  -- 1 
+        # 5: left = 0 , right =0 --- 1
+        # 3: left = 0, right =0 --- 1
         
         
         
