5c4f683c1a6841c50a72638d740c8bf6667c02be
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-11-05 19:40:57 -0400

    Time: 133 ms (98.35%), Space: 14.2 MB (98.23%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,25 +1,31 @@
-class Solution(object):
-    def longestStrChain(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
-        words.sort(key = len) # 根据长度来sort这个words list
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        # sort - increasing order (len)
+        # a: 1
+        # b: 1
+        # ba:1 +1
+        # bca: 3
+        # bda: 1
+        # bdca:4
+        words.sort(key = len)
         d = collections.defaultdict(int)
         for word in words:
             d[word] = 1
-            
+        
         for word in words:
-            cur_word = word
-            for i in range(len(cur_word)):
-                prev_word = cur_word[:i] + cur_word[i+1:]
-                if prev_word in d:
-                    d[cur_word] = max(d[prev_word]+1, d[cur_word])
+            for i in range(len(word)):
+                substring = word[:i] + word[i+1:]
+                if substring in d:
+                    d[word] = max(d[word], d[substring] + 1)
         return max(d.values())
         
-        # a:1
-        # b:1
-        # ba:2
-        # bca:3
-        # bda:3
-        # bdca:4
+        
+    
+    
+                
+        
+        
+        
+        
+        
+       
