9d0260acf9d43fc4f0b0cb2610f9cee697f3ca16
Sundri Bun <77870658+yantinglai@users.noreply.github.com>
2022-11-06 00:33:59 -0400

    Time: 1888 ms (6.75%), Space: 33.1 MB (5.09%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -5,14 +5,13 @@ class Solution:
         # can use a dp array to store the value 
         m = len(matrix)
         n = len(matrix[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp = [[1 for _ in range(n)] for _ in range(m)]
         # print(dp)
-        
-        
+    
         def dfs(r,c,prev_val):
             if r < 0 or c < 0 or r >= len(matrix) or c >= len(matrix[0]) or matrix[r][c] <= prev_val:
                 return 0
-            if dp[r][c] != 0:  # if it is already visited, return this value
+            if dp[r][c] != 1:  # if it is already visited, return this value
                 return dp[r][c] # where do you initialize the res?
             
             res = 1
@@ -20,6 +19,7 @@ class Solution:
             res = max(res, dfs(r-1,c,matrix[r][c]) + 1)
             res = max(res, dfs(r,c+1,matrix[r][c]) + 1)
             res = max(res, dfs(r,c-1,matrix[r][c]) + 1)
+            
             dp[r][c] = res
             return res
         
